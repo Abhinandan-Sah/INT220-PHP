0c4e3e24686d2ed06c17ac1c3652a8213a3edf76
Abhinandan Sah <abhinandansah222@gmail.com>
2024-08-22 15:26:56 +0530

    datatype in php

---
+++ 2-datatype.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Data</h1>
+    <?php
+    $a=26;
+    $b=2.6;
+    echo "this is value $a";
+    echo "this is value $b";
+    ?>
+</body>
+</html>
