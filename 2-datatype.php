431e9c4a24fcd76f413ecd6fce4c192861fd49dd
Abhinandan Sah <abhinandansah222@gmail.com>
2024-08-23 23:55:39 +0530

    leap year in php

--- 2-datatype.php
+++ 2-datatype.php
@@ -6,12 +6,43 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Data</h1>
+    <marquee><h1>Datatype</h1></marquee>
+    <h1>Scalar Datatype</h1>
     <?php
     $a=26;
     $b=2.6;
-    echo "this is value $a";
-    echo "this is value $b";
+    $c="hey";
+    $d=false;
+    echo "this is value $a "."<br>";
+    echo "this is value $b"."<br>";
+    echo "this is value $c"."<br>";
+    echo "this is value $d"."<br>";
+
     ?>
+    <h1>Compound Datatype</h1>
+    <?php
+    $a=[23, 64, 30];
+    // $b=2.6;
+    // $c="hey";
+    // $d=false;
+    echo "this is value $a[0]"."<br>";
+    // echo "this is value $b"."<br>";
+    // echo "this is value $c"."<br>";
+    // echo "this is value $d"."<br>";
+    ?>
+
+<?php
+
+$person = [
+    "name" => "avi",
+    "age" => 22,
+    "city" => "Newtop"
+];
+echo $person["name"];
+echo $person["age"];
+echo $person["city"];
+
+?>
+
 </body>
 </html>
