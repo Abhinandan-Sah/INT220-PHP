56046e71fb88bb3a2758eb890a3ea94edf04bfd2
Abhinandan Sah <abhinandansah222@gmail.com>
2024-08-30 16:15:07 +0530

    uo

---
+++ 4-forEach.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <label for="value1">Enter value for 1</label>
+        <input type="number" /><br />
+        <label for="value1">Enter value for 1</label>
+        <input type="number" /><br />
+        <button onclick="calculate">calculate</button>
+    </div>
+    <?php
+
+        $colors = array("Red", "Green", "Blue");
+ 
+        // Loop through colors array
+        foreach($colors as $value){
+            echo $value . "<br>";
+        }
+
+        foreach($colors as $value){
+            echo $
+        }
+        
+    ?>
+</body>
+</html>
