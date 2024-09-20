883904575d071c506628e6cde6726024757f538e
Abhinandan Sah <abhinandansah222@gmail.com>
2024-09-20 15:50:21 +0530

    form in php

--- server.php
+++ server.php
@@ -1,7 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
-    echo "Hello, " . $name . "!";
+    $email = htmlspecialchars($_POST['email']);
+    echo "Hello, " . $name . "!<br>";
+    echo "Your email id is, " . $email . "!";
 } else {
     echo "Please submit the form.";
 }
